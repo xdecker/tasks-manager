169b1637054fb7ebe0993a1d24802b565c27a07d
xdecker <xavier_deckza@hotmail.com>
2026-03-13 20:19:42 -0500

    fix: update schema on auth.api

--- tasks-mobile/src/features/auth/api/auth.api.ts
+++ tasks-mobile/src/features/auth/api/auth.api.ts
@@ -1,16 +1,14 @@
 import { apiClient } from "../../../api/client";
-import {
-  LoginRequest,
-  LoginResponse,
-  SignUpRequest,
-} from "@/features/auth/types/auth.types";
+import { LoginResponse } from "@/features/auth/types/auth.types";
+import { LoginFormData } from "../schemas/login.schema";
+import { SignupFormData } from "../schemas/signup.schema";
 
-export async function loginRequest(data: LoginRequest) {
+export async function loginRequest(data: LoginFormData) {
   const response = await apiClient.post<LoginResponse>("/auth/login", data);
   return response.data;
 }
 
-export async function signupRequest(data: SignUpRequest) {
+export async function signupRequest(data: SignupFormData) {
   const response = await apiClient.post<LoginResponse>("/auth/signup", data);
   return response.data;
 }
